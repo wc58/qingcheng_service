a2b9505541752fcc58c5985f4f7222ea7cc09c42
wc58 <2258354832@qq.com>
2020-02-22 20:49:52 +0800

    1、新增OSS云存储

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
@@ -1,7 +1,9 @@
 package com.qingcheng.controller.file;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.qingcheng.controller.utils.ConstantPropertiesUtils;
 import org.joda.time.DateTime;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,18 +13,26 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
 
+    /**
+     * 上传到本地服务器
+     *
+     * @param file
+     * @param request
+     * @return
+     */
     @RequestMapping("/native")
     public String nativeUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
         //真实路径
         String path = request.getSession().getServletContext().getRealPath("img");
         //文件及其目录
-        String fileName = new DateTime().toString("yyyy/MM/dd") + "/" + UUID.randomUUID() + file.getOriginalFilename();
+        String fileName = new DateTime().toString("yyyy/MM/dd") + "/" /*+ UUID.randomUUID()*/ + file.getOriginalFilename();
         String filePath = path + "/" + fileName;
         //目标文件
         File desFile = new File(filePath);
@@ -35,6 +45,34 @@ public class UploadController {
             e.printStackTrace();
         }
         return "http://127.0.0.1:9101/img/" + fileName;
+    }
+
+    /**
+     * 上传到OSS服务器
+     *
+     * @param file
+     * @return
+     */
+    @PostMapping("OSS")
+    public String OSSUpload(@RequestParam("file") MultipartFile file) {
+
+        //OSS连接参数
+        String endpoint = ConstantPropertiesUtils.ENDPOINT;
+        String accessKeyId = ConstantPropertiesUtils.ACCESS_KEY_ID;
+        String accessKeySecret = ConstantPropertiesUtils.ACCESS_KEY_SECRET;
+        String bucketName = ConstantPropertiesUtils.BUCKET_NAME;
+
+        try {
+            //创建实例
+            OSS ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+            String filename = new DateTime().toString("yyy/MM/dd") + "/" + UUID.randomUUID() + file.getOriginalFilename();
+            InputStream inputStream = file.getInputStream();
+            ossClient.putObject(bucketName, filename, inputStream);
+            return "https://" + bucketName + "." + endpoint + "/" + filename;
+        } catch (IOException e) {
+//            e.printStackTrace();
+            return "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=182992324,4078619122&fm=26&gp=0.jpg";
+        }
     }
 
 }
