5c2596dd7d0274e95c0406c7ad66428b9851d869
wc58 <2258354832@qq.com>
2020-02-27 14:58:19 +0800

    1、优化表统计页面显示

--- qingcheng_service_order/src/main/java/com/qingcheng/dao/CategoryReportMapper.java
+++ qingcheng_service_order/src/main/java/com/qingcheng/dao/CategoryReportMapper.java
@@ -35,7 +35,7 @@ public interface CategoryReportMapper extends BaseMapper<CategoryReport> {
 
     @Select("SELECT category_id1 categoryId1, c.`name` name ,SUM(num) num,SUM(money) money\n" +
             "FROM tb_category_report r,tb_category1 c\n" +
-            "WHERE count_date>=#{startDate} AND count_date<=#{endDate}\n" +
+            "WHERE r.category_id1 = c.id ANd count_date>=#{startDate} AND count_date<=#{endDate}\n" +
             "GROUP BY category_id1,c.`name`")
     public List<Map<String,String>> category1Count(@Param("startDate") String startDate, @Param("endDate") String endDate);
 }
